686c70ba755c49cf7acc3b449fc22fd4a9b2ba37
skrowll <lucasdejesus.rocha@hotmail.com>
2023-08-15 16:27:31 -0300

    revert: revert last commit

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -9,13 +9,11 @@ export const setToken = (token) => {
 };
 
 export const requestData = async (endpoint) => {
-  api.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
   const { data } = await api.get(endpoint);
   return data;
 };
 
 export const requestLogin = async (endpoint, body) => {
-  api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
   const { data } = await api.post(endpoint, body);
   return data;
 };
